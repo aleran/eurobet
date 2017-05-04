9a62ac5d71a285a68c405dd585a78d6e38529ccf
aleran <ale.ran92@gmail.com>
2017-05-04 02:24:26 -0400

    suma saldo solo si es ticket de usuario normal

--- accion_ticket.php
+++ accion_ticket.php
@@ -10,21 +10,34 @@
 
 	if (isset($_GET["ganar"])) {
 
-		$sql_ganar="UPDATE parlay SET ganar='1' WHERE codigo='".$_GET["ganar"]."'";
-		$rs_ganar=mysqli_query($mysqli,$sql_ganar) or die(mysqli_error($mysqli));
-		
 		$sql_premio="SELECT premio, cedula FROM parlay WHERE codigo='".$_GET["ganar"]."'";
 		$rs_premio=mysqli_query($mysqli,$sql_premio) or die(mysqli_error($mysqli));
 		$row_premio=mysqli_fetch_array($rs_premio);
 
-		$sql_usuario="SELECT saldo FROM usuarios WHERE cedula='".$row_premio["cedula"]."'";
-		$rs_usuario=mysqli_query($mysqli,$sql_usuario) or die(mysqli_error($mysqli));
-		$row_usuario=mysqli_fetch_array($rs_usuario);
+		if ($row_premio["cedula"]=="") {
+			$sql_ganar="UPDATE parlay SET ganar='1' WHERE codigo='".$_GET["ganar"]."'";
+			$rs_ganar=mysqli_query($mysqli,$sql_ganar) or die(mysqli_error($mysqli));
+			echo "<script>alert('ticket marcado como Ganador');window.location='activos.php?desde=".$_GET["desde"]."&hasta=".$_GET["hasta"]."'</script>";
 
-		$saldo_f= $row_premio["premio"] + $row_usuario["saldo"];
-		$sql_saldo="UPDATE usuarios SET saldo='".$saldo_f."' WHERE cedula='".$row_premio["cedula"]."'";
-		$rs_saldo=mysqli_query($mysqli,$sql_saldo) or die(mysqli_error($mysqli));
-		echo "<script>alert('ticket marcado como Ganador');window.location='activos.php?desde=".$_GET["desde"]."&hasta=".$_GET["hasta"]."'</script>";
+		}
+
+		else {
+			
+			$sql_ganar="UPDATE parlay SET ganar='1' WHERE codigo='".$_GET["ganar"]."'";
+			$rs_ganar=mysqli_query($mysqli,$sql_ganar) or die(mysqli_error($mysqli));
+
+			$sql_usuario="SELECT saldo FROM usuarios WHERE cedula='".$row_premio["cedula"]."'";
+			$rs_usuario=mysqli_query($mysqli,$sql_usuario) or die(mysqli_error($mysqli));
+			$row_usuario=mysqli_fetch_array($rs_usuario);
+
+			$saldo_f= $row_premio["premio"] + $row_usuario["saldo"];
+			$sql_saldo="UPDATE usuarios SET saldo='".$saldo_f."' WHERE cedula='".$row_premio["cedula"]."'";
+			$rs_saldo=mysqli_query($mysqli,$sql_saldo) or die(mysqli_error($mysqli));
+			echo "<script>alert('ticket marcado como Ganador');window.location='activos.php?desde=".$_GET["desde"]."&hasta=".$_GET["hasta"]."'</script>";
+		}
+		
+		
+		
 
 
 	}
