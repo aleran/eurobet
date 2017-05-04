cfe55b832fc103b3cbb0b914ae3340b72b4389da
aleran <ale.ran92@gmail.com>
2017-05-04 01:56:26 -0400

    se suma al saldo cuando es ganandor

--- accion_ticket2.php
+++ accion_ticket2.php
@@ -5,13 +5,25 @@
 
 		$sql_anular="UPDATE parlay SET activo='0' WHERE codigo='".$_GET["anular"]."'";
 		$rs_anular=mysqli_query($mysqli,$sql_anular) or die(mysqli_error($mysqli));
-		echo "<script>alert('ticket marcado como Ganador');window.location='consultas.php'</script>";
+		echo "<script>alert('ticket anulado');window.location='consultas.php'</script>";
 	}
 
 	if (isset($_GET["ganar"])) {
 
 		$sql_ganar="UPDATE parlay SET ganar='1' WHERE codigo='".$_GET["ganar"]."'";
 		$rs_ganar=mysqli_query($mysqli,$sql_ganar) or die(mysqli_error($mysqli));
+
+		$sql_premio="SELECT premio, cedula FROM parlay WHERE codigo='".$_GET["ganar"]."'";
+		$rs_premio=mysqli_query($mysqli,$sql_premio) or die(mysqli_error($mysqli));
+		$row_premio=mysqli_fetch_array($rs_premio);
+
+		$sql_usuario="SELECT saldo FROM usuarios WHERE cedula='".$row_premio["cedula"]."'";
+		$rs_usuario=mysqli_query($mysqli,$sql_usuario) or die(mysqli_error($mysqli));
+		$row_usuario=mysqli_fetch_array($rs_usuario);
+
+		$saldo_f= $row_premio["premio"] + $row_usuario["saldo"];
+		$sql_saldo="UPDATE usuarios SET saldo='".$saldo_f."' WHERE cedula='".$row_premio["cedula"]."'";
+		$rs_saldo=mysqli_query($mysqli,$sql_saldo) or die(mysqli_error($mysqli));
 		echo "<script>alert('ticket marcado como Ganador');window.location='consultas.php'</script>";
 	}
 
