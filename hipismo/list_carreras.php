315470d92fe1f02123b1581884dd493ef42e8b53
alejandro <ale.ran92@gmail.com>
2017-10-08 01:05:16 -0500

    lista de evalauar carreras

---
+++ hipismo/list_carreras.php
@@ -0,0 +1,250 @@
+<?php include("../time_sesion.php");
+if ($_SESSION['tipo']=="normal") {
+    header("Location: ../bienvenido.php");
+} 
+    include("../conexion/conexion.php");  
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head><meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+
+    <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+        <meta name="description" content="Sitio de Apuestas en colombia, Parlays, Apuestas directas">
+        <meta name="author" content="">
+        <title>EUROBET :: Tu sitio de apuestas parlay</title>
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../fonts/style.css" rel="stylesheet">
+        <link href="../css/style.css" rel="stylesheet">
+        <link href="../pacejs/themes/orange/pace-theme-barber-shop.css" rel="stylesheet">
+        <link rel="icon"  href="balon.ico">
+
+
+
+</head>
+
+<body>
+ 
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <!-- Menu -->
+        <?php 
+            include("../menu2.php");
+        ?>
+</div>
+
+        <!-- /#sidebar-wrapper -->
+       
+        <!-- Contenido -->
+        <div id="page-content-wrapper">
+            <header>
+                <img src="img/header3.png" class="img-responsive" alt="">
+        </header>
+        <br>
+            <div class="container-fluid">
+                <div align="center" class="visible-xs"><a href="#menu-toggle" class="btn btn-info menu-toggle"><span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span> Menu</a></div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <?php 
+                          $sql_ag="SELECT agencia FROM agencias WHERE id='".$_SESSION["agencia"]."'";
+                            $rs_ag=mysqli_query($mysqli,$sql_ag);
+                            $row_ag=mysqli_fetch_array($rs_ag);
+                            echo "<h4>Agencia: ". $row_ag["agencia"]; 
+                        ?> 
+                            <a href="cerrar_sesion.php"> Salir</a></h4>
+                    </div>
+                    
+                </div>
+                
+                <div class="row">
+
+                    <h3><center><b>Partidos para Evaluar:</b></center></h3><br>
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <th>Hipodromo</th>
+                                <th>Carrera</th>
+                                <th>Fecha - Hora</th>
+                            </thead>
+                            <tbody>
+                                
+                    
+                    <?php               
+                        
+                            $sql_carreras="SELECT c.id, c.id_nc, n.carrera, n.fecha, n.hora, n.id_hipodromo FROM carreras c JOIN nombre_carreras n ON c.id_nc=n.id WHERE n.inicio='1' AND n.eval='0' ORDER BY n.fecha, n.hora ASC";
+            
+                        $rs_carreras=mysqli_query($mysqli, $sql_carreras) or die(mysqli_error());
+                        while ($row_carreras=mysqli_fetch_array($rs_carreras)) {
+
+                                $sql_h="SELECT hipodromo FROM hipodromos WHERE id='".$row_carreras["id_hipodromo"]."'";
+                                $rs_h=mysqli_query($mysqli, $sql_h) or die(mysqli_error());
+                                $row_h=mysqli_fetch_array($rs_h);
+
+
+                                list($a3,$m3,$d3) = explode("-", $row_carreras["fecha"]);
+                                $fecha=$d3."/".$m3."/".$a3;
+                                echo"<tr>";
+                                    echo"<td>";
+                                        echo $row_h["hipodromo"];
+                                    echo"</td>";
+                                     echo"<td>";
+                                        echo "<a href='eval_carreras.php?carrera=".$row_carreras["id_nc"]."'>".$row_carreras["carrera"]."</a>";
+                                    echo"</td>";
+                                    echo"<td>";
+                                        echo $fecha ." - ". $row_carreras["hora"];
+                                    echo"</td>";
+                                echo"</tr>";
+
+                        }
+
+                    ?>
+                            
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                
+            
+                <br>
+        
+            </div>
+            
+            <!-- Modal Crear de Usuarios -->
+
+            <div class="modal fade" id="modalUsuarios" tabindex="-1" role="dialog" aria-labelledby="modalUsuariosLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="modalUsuariosLabel">Registro de Usuarios</h4>
+                        </div>
+                        <div class="modal-body">
+                        
+                            <form class="form-horizontal" method="POST" action="crear_usuarios.php">
+                                <div class="form-group">
+                                    <label for="pais" class="col-sm-4 control-label">Pais:</label>
+                                    <div class="col-sm-6">
+                                        <select  name="pais" id="pais" class="form-control">
+                                        <?php 
+                                            $sql_pais="SELECT * FROM paises";
+                                            $rs_pais=mysqli_query($mysqli,$sql_pais) or die(mysqli_error());
+                                            while ($row_pais=mysqli_fetch_array($rs_pais)) {
+                                                echo  '<option value='.$row_pais["id"].'>'.$row_pais["pais"].'</option>';
+                                            }
+
+                                        ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                 <div class="form-group">
+                                    <label for="agencia" class="col-sm-4 control-label">Agencia:</label>
+                                    <div class="col-sm-6">
+                                        <select  name="agencia" id="agencia" class="form-control">
+                                        <?php 
+                                            $sql_agencias="SELECT * FROM agencias";
+                                            $rs_agencias=mysqli_query($mysqli,$sql_agencias) or die(mysqli_error());
+                                            while ($row_agencias=mysqli_fetch_array($rs_agencias)) {
+                                                echo  '<option value='.$row_agencias["id"].'>'.$row_agencias["agencia"].'</option>';
+                                            }
+
+                                        ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="tipo" class="col-sm-4 control-label">Tipo de usuario:</label>
+                                    <div class="col-sm-6">
+                                        <select  name="tipo" id="tipo" class="form-control">
+                                            <option value="admin">Admin</option>
+                                            <option value="taquilla">Taquilla</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="cedula" class="col-sm-4 control-label">Cédula:</label>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" name="cedula" id="cedula" placeholder="" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="nombre" class="col-sm-4 control-label">Nombre:</label>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" name="nombre" id="nombre" placeholder="" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="apellido" class="col-sm-4 control-label">Apellido:</label>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" name="apellido" id="apellido" placeholder="" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="correo" class="col-sm-4 control-label">Correo:</label>
+                                    <div class="col-sm-6">
+                                        <input type="email" class="form-control" name="correo" id="correo" placeholder="" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="clave" class="col-sm-4 control-label">Password:</label>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control" name="clave" id="clave" placeholder="" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="direccion" class="col-sm-4 control-label">Dirección:</label>
+                                    <div class="col-sm-6">
+                                        <textarea class="form-control" name="direccion" id="direccion"  rows="3" required=""></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="telefono" class="col-sm-4 control-label">Teléfono:</label>
+                                    <div class="col-sm-6">
+                                        <input type="tel" class="form-control" name="telefono" id="telefono" placeholder="" required>
+                                    </div>
+                                </div>
+                            
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                            <button class="btn btn-success">Crear Usuario</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            
+
+          
+
+
+            <!-- Contenido -->
+            
+        </div>
+
+
+        
+
+
+
+
+    
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Menu Toggle Script -->
+    <script>
+    $(".menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+</body>
+
+
+</html>
---
+++ hipismo/menu_c1.php
@@ -0,0 +1,72 @@
+<?php
+	echo '<div id="sidebar-wrapper" class="">
+            <ul class="sidebar-nav">
+            <div align="center" class="visible-xs"><a href="#menu-toggle" class="btn btn-info menu-toggle">Cerrar Menú</a></div>';
+                    echo '<li class="sidebar-brand">
+                        <a href="#">
+                           <span class="icon-home"></span>&nbsp;Sesion Iniciada
+                        </a>
+                    </li>';
+
+                    
+                echo '<li>
+                    
+                    <a href="bienvenido.php" title="Bienvenido a Eurobet"><span class="icon-checkmark2"></span>&nbsp;Inicio</a>
+                </li>';
+                if ($_SESSION["tipo"]=="root" || $_SESSION["tipo"]=="admin" ) {
+                 echo '<li>
+                        <a href="buscar_usuario.php" title="Recarga de saldo"><span class="icon-coin-dollar"></span>&nbsp;Recargas</a>
+                        </li>';
+                }
+                    if ($_SESSION["tipo"]=="root") {
+                        echo '<li>
+                        <a href="#" title="crear usuarios" data-toggle="modal" data-target="#modalUsuarios"><span class="icon-user-tie"></span>&nbsp;Crear Usuarios</a>
+                        </li>';
+
+                        echo '<li>
+                        <a href="list_partidos.php" title="Evaluacion de Partidos"><span class="icon-checkmark2"></span>&nbsp;Evaluar Partidos</a>
+                        </li>';
+                    
+                   
+                    
+                    }
+                
+                
+                    echo '<li>
+                        <a href="competiciones.php" title="Apuesta"><span class="icon-coin-dollar"></span>&nbsp;Apostar</a>
+                    </li>';
+
+                    
+                    echo '<li>
+                        <a href="consultas.php" title="Consulta de Tickets Activos"><span class="icon-file-text2"></span>&nbsp;Tickets</a>
+                    </li>';
+                    if ($_SESSION["tipo"]=="root" || $_SESSION["tipo"]=="admin" ) {
+                    echo '<li>
+                                <a href="saldos.php" title="Resumen Económico detallado de Agencias"><span class="icon-coin-dollar"></span>&nbsp;Cuentas</a>
+                            </li>';
+                    }
+                    echo '<li>
+                                <a href="cambiar_clave.php" title="Cambiar Contraseña"><span class="icon-checkmark2"></span>&nbsp;Cambiar Clave</a>
+                            </li>';
+                
+                echo '<li>
+                    <a href="reglas-de-juego.html" target="_blank"  title="Conoce nuestras políticas y términos de prestación de Servicio"><span class="icon-library"></span>&nbsp;Reglas</a>
+                            
+                </li>
+                <li>
+                    <a href="ayuda.html" target="_blank" title="¿Necesitas ayuda? , Comunícate con nosotros"><span class="icon-bullhorn"></span>&nbsp;¿Cómo Apostar?</a>
+                </li>
+                <li>
+                    <a href="juego-responsable.html" target="_blank" title="Juego Responsable"><span class="icon-hammer2"></span>&nbsp;Responsabilidad</a>
+                </li>
+                <li>
+                    <a href="formulario_contacto.html" target="_blank" title="PQR - Peticiones, Quejas y Reclamos"><span class="icon-pushpin"></span>&nbsp;Contáctenos</a>
+                </li>
+                <li>
+                    <a href="descargas.html" target="_blank" title="Descarga de Software para taquillas"><span class="icon-download"></span>&nbsp;Descargas</a>
+                </li>
+                
+               
+            </ul>
+        </div>';
+?>
